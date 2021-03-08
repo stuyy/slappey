dee3f53d375855f578b9b980def7094158b09272
loveisglitchy <72410854+loveisglitchy@users.noreply.github.com>
2021-03-08 14:50:52 -0600

    Update templates.ts
    Just adding "async" to commands to negate "await" issues.

--- src/templates/templates.ts
+++ src/templates/templates.ts
@@ -174,7 +174,7 @@ export default abstract class BaseCommand {
   getCategory(): string { return this.category; }
   getAliases(): Array<string> { return this.aliases; }
 
-  abstract run(client: DiscordClient, message: Message, args: Array<string> | null): Promise<void>;
+  abstract async run(client: DiscordClient, message: Message, args: Array<string> | null): Promise<void>;
 }`;
 }
 
